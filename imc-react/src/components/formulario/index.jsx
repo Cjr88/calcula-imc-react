baf10ee172266689f72f70aa8992926b6e70cc27
Cjr88 <carloscjr70@gmail.com>
2023-06-01 17:43:01 -0300

    correção na logica do js, no calculo do imc

--- imc-react/src/components/formulario/index.jsx
+++ imc-react/src/components/formulario/index.jsx
@@ -1,38 +1,50 @@
 import { useState } from "react";
 
+
 const Formulario = () => {
 
-    const [peso, setPeso] = useState(0);
-    const [altura, setAltura] = useState(0);
-    const [resultadoImc, setResultadoImc] = useState('');
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [result, setResult] = useState('')
 
-    const calculaImc = (peso, altura) => {
-        altura = altura / 100;
+    const calculaImc = () => {
         const imc = peso / (altura * altura);
-
+        const imcFormatado = imc.toFixed(2);
+        setResult(+imcFormatado)
         if (imc < 17) {
-            setResultadoImc(` Muito abaixo do peso!`);
+            setResult(`De acordo com a tabela do IMC o seu peso se encontra muito abaixo do peso, tenha cuidado! Esse peso é prejudicial a sua saúde!`);
         } else if (imc >= 17 && imc <= 18.49) {
-            setResultadoImc(`Abaixo do peso!`);
+            setResult('De acordo com a tabela do IMC o seu peso se encontra abaixo do peso. Se alimente melhor! ');
         } else if (imc >= 18.5 && imc <= 24.99) {
-            setResultadoImc(`Peso normal!`);
+            setResult('De acordo com a tabela do IMC o seu peso se encontra normal. Continue assim!');
         } else if (imc >= 25 && imc <= 29.99) {
-            setResultadoImc(`Acima do peso!`);
+            setResult('De acordo com a tabela do IMC o seu peso se encontra acima do peso. Atenção redobrada na sua alimentação!');
         } else if (imc >= 30 && imc <= 34.99) {
-            setResultadoImc(`Obesidade I`);
+            setResult('De acordo com a tabela do IMC o seu peso se encontra em um grau de Obesidade I. Tenha cuidado esse peso é muito prejudicial a saúde!');
         } else {
-            setResultadoImc(`Obesidade II`);
+            setResult('De acordo com a tabela do IMC o seu peso se encontra em um grau de Obesidade II. Procure a ajuda de um profissional!');
         }
 
     }
 
     return (
-        <form >
-            <input type="number" placeholder="Peso" value={peso} onChange={evento => setPeso(parseFloat(evento.target.value))} />
-            <input type="number" placeholder="Altura" value={altura} onChange={evento => setAltura(parseFloat(evento.target.value))} />
-            <button type="submit" onClick={calculaImc}>Calcular</button> 
-            <p>{resultadoImc}</p>
-        </form>
+        <div className="container">
+            <input
+                type="number"
+                placeholder="Peso (KG)"
+                value={peso}
+                onChange={(e) => setPeso(parseFloat(+e.target.value))}
+            />
+            <input
+                type="number"
+                placeholder="Altura (M)"
+                value={altura}
+                onChange={(e) => setAltura(parseFloat(+e.target.value))}
+            />
+            <button onClick={calculaImc} >Calcular</button>
+            <p className="resultado">{result}</p>
+        </div>
+
     )
 }
 export default Formulario;
